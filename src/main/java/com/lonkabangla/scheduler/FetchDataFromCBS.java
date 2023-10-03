cf92e053cb71e43a7bb26b490617430301b9c823
ajharulabedeen <ajharulabedeen@gmail.com>
2023-10-03 14:32:25 +0600

    feat: entity manager.

--- src/main/java/com/lonkabangla/scheduler/FetchDataFromCBS.java
+++ src/main/java/com/lonkabangla/scheduler/FetchDataFromCBS.java
@@ -18,11 +18,11 @@ public class FetchDataFromCBS {
     @PersistenceContext
     private EntityManager entityManager;
 
-    @Transactional
     public void fetchCollectionData() {
         String fetchCollection = "INSERT INTO DBLINK_LOAN_OVERDUE_COLLECTIONS_ SELECT * FROM ultimus.VW_Dashboard_Loan_Overdue_Collections@dashboardDBLINK1 t WHERE t.lon_dept_id = 4;\n";
 
         try {
+            entityManager.getTransaction().begin();
             int executeUpdate = entityManager.createNativeQuery(fetchCollection)
                     .executeUpdate();
             if (executeUpdate > 0) {
@@ -30,16 +30,17 @@ public class FetchDataFromCBS {
             } else {
                 logger.error(" Colelction Data Fetch Failed!");
             }
+            entityManager.getTransaction().commit();
         } catch (Exception e) {
             logger.error(e.getStackTrace().toString());
         }
     }
 
-    @Transactional
     public void fetchDPS() {
         String fetchDPS = "INSERT INTO DBLINK_DPS_ select * from ultimus.VW_DASHBOARD_DPS_AC@dashboardDBLINK1 t where t.RO_ID = '1180101525' or t.RM_ID = '1180101525'";
 
         try {
+            entityManager.getTransaction().begin();
             int executeUpdate = entityManager.createNativeQuery(fetchDPS)
                     .executeUpdate();
             if (executeUpdate > 0) {
@@ -47,16 +48,18 @@ public class FetchDataFromCBS {
             } else {
                 logger.error(" DPS Data Fetch Failed!");
             }
+            entityManager.getTransaction().commit();
         } catch (Exception e) {
             logger.error(e.getStackTrace().toString());
         }
     }
 
-    @Transactional
+
     public void fetchTDR() {
         String fetchDPS = "INSERT INTO DBLINK_TDR_ select * from ultimus.VW_DASHBOARD_TDR_AC@dashboardDBLINK1 t where t.RO_ID = '1180101525' or t.RM_ID = '1180101525'";
 
         try {
+            entityManager.getTransaction().begin();
             int executeUpdate = entityManager.createNativeQuery(fetchDPS)
                     .executeUpdate();
             if (executeUpdate > 0) {
@@ -64,6 +67,7 @@ public class FetchDataFromCBS {
             } else {
                 logger.error(" TDR Data Fetch Failed!");
             }
+            entityManager.getTransaction().commit();
         } catch (Exception e) {
             logger.error(e.getStackTrace().toString());
         }
