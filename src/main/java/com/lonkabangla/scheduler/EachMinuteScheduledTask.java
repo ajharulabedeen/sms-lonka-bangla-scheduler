3a492609967eeae66e7127bb8d8c59cd8245ae0a
ajharulabedeen <ajharulabedeen@gmail.com>
2023-10-03 12:18:04 +0600

    feat: fetch data added.

--- src/main/java/com/lonkabangla/scheduler/EachMinuteScheduledTask.java
+++ src/main/java/com/lonkabangla/scheduler/EachMinuteScheduledTask.java
@@ -1,5 +1,6 @@
 package com.lonkabangla.scheduler;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -7,10 +8,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class EachMinuteScheduledTask {
 
+    @Autowired
+    FetchDataFromCBS fetchDataFromCBS;
+
     // This method will be executed every minute
-    @Scheduled(cron = "0 * * * * ?")
+//    @Scheduled(cron = "0 * * * * ?")
+    // This method will be executed every 2 minute
+    @Scheduled(cron = "0 */2 * * * ?")
     public void runTask() {
         // Define the task you want to perform here
         System.out.println("Scheduled task is Every Minute: ");
+        fetchDataFromCBS.fetchCollectionData();
+        fetchDataFromCBS.fetchDPS();
+        fetchDataFromCBS.fetchTDR();
     }
 }
---
+++ src/main/java/com/lonkabangla/scheduler/FetchDataFromCBS.java
@@ -0,0 +1,75 @@
+package com.lonkabangla.scheduler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Component
+@Slf4j
+public class FetchDataFromCBS {
+
+    private final Logger logger = LoggerFactory.getLogger(FetchDataFromCBS.class);
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public void fetchCollectionData() {
+        String fetchCollection = "INSERT INTO DBLINK_LOAN_OVERDUE_COLLECTIONS SELECT * FROM ultimus.VW_Dashboard_Loan_Overdue_Collections@dashboardDBLINK1 t WHERE t.lon_dept_id = 4;\n";
+
+        try {
+            int executeUpdate = entityManager.createNativeQuery(fetchCollection)
+                    .executeUpdate();
+            if (executeUpdate > 0) {
+                logger.info(" Colelction Data Fetch Success!");
+            } else {
+                logger.error(" Colelction Data Fetch Failed!");
+            }
+        } catch (Exception e) {
+            logger.error(e.getStackTrace().toString());
+        }
+    }
+
+    @Transactional
+    public void fetchDPS() {
+        String fetchDPS = "INSERT INTO DBLINK_DPS select * from ultimus.VW_DASHBOARD_DPS_AC@dashboardDBLINK1 t where t.RO_ID = '1180101525' or t.RM_ID = '1180101525'";
+
+        try {
+            int executeUpdate = entityManager.createNativeQuery(fetchDPS)
+                    .executeUpdate();
+            if (executeUpdate > 0) {
+                logger.info(" DPS Data Fetch Success!");
+            } else {
+                logger.error(" DPS Data Fetch Failed!");
+            }
+        } catch (Exception e) {
+            logger.error(e.getStackTrace().toString());
+        }
+    }
+
+    @Transactional
+    public void fetchTDR() {
+        String fetchDPS = "INSERT INTO DBLINK_TDR select * from ultimus.VW_DASHBOARD_TDR_AC@dashboardDBLINK1 t where t.RO_ID = '1180101525' or t.RM_ID = '1180101525'";
+
+        try {
+            int executeUpdate = entityManager.createNativeQuery(fetchDPS)
+                    .executeUpdate();
+            if (executeUpdate > 0) {
+                logger.info(" TDR Data Fetch Success!");
+            } else {
+                logger.error(" TDR Data Fetch Failed!");
+            }
+        } catch (Exception e) {
+            logger.error(e.getStackTrace().toString());
+        }
+    }
+}
+
+
+
+
